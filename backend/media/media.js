705dd9750d45d93276cd3011531312d30ae816b2
Vikram Kumar <vikramkumar9211903@gmail.com>
2025-02-07 18:38:28 +0530

    update on video

--- backend/media/media.js
+++ backend/media/media.js
@@ -4,7 +4,7 @@ import ensureDirectoryExistence from "../utils/ensureDirectoryExistence.js";
 
 export async function videosData({ inputPath, outputPath }) {
   ensureDirectoryExistence(path.dirname(outputPath));
-  const ffmpegCommand = `ffmpeg -i "${inputPath}"  -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${path.dirname(
+  const ffmpegCommand = `ffmpeg -i "${inputPath}" -preset ultrafast -threads 2 -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${path.dirname(
     outputPath
   )}/segment%03d.ts" -start_number 0 "${outputPath}"`;
   await executeCommand(ffmpegCommand);
