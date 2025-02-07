3eaebd9b87c28b6a21b43baf5aa7cc04808ee557
Vikram Kumar <vikramkumar9211903@gmail.com>
2025-02-07 05:30:31 +0530

    updated in frontend in main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,17 @@
-import { StrictMode, lazy, Suspense } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import MediaContext from "./context/MediaContext.jsx";
 import ThemeProvider from "./context/ThemeContext.jsx";
-
-const App = lazy(() => import("./App.jsx")); // Lazy load App
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
       <MediaContext>
         <BrowserRouter>
-          <Suspense fallback={<div>Loading...</div>}>
-            <App />
-          </Suspense>
+          <App />
         </BrowserRouter>
       </MediaContext>
     </ThemeProvider>
